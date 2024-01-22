7a9b7bff7ca180bf07bde11e516c46d2ceefd6b4
anesh.psk <anesh.psk@zohocorp.com>
2024-01-22 18:23:31 +0530

    dwdwdwd

--- BlobForwarder/__init__.py
+++ BlobForwarder/__init__.py
@@ -22,7 +22,7 @@ except Exception as e:
     print(str(e))
 
 try:
-    timestamp = os.environ["CollectionTime"]
+    timestamp = os.environ["LogCollectionStartTime"]
 except Exception as e:
     print(str(e))
 
